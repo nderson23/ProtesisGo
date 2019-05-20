5ca8a20bcd9139cf8b5c0713ee8f7669de4c0780
Anderson Torres Bedoya <anderson.torres@udea.edu.co>
2019-05-19 20:20:50 -0500

    Actualización del proyecto

---
+++ SonrisaPerfecta/Controllers/RegistrarPacienteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SonrisaPerfecta.Controllers
+{
+    public class RegistrarPacienteController : Controller
+    {
+        //
+        // GET: /RegistrarPaciente/
+        public ActionResult RegistrarPaciente()
+        {
+            ViewBag.Title = "Registrar paciente";
+
+            return View();
+        }
+	}
+}
